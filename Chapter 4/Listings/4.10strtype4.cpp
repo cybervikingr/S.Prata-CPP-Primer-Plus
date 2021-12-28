a4ec8b9d5c5a84197318d107adfe79bef1f5afca
cybervikingr <cyberviking@tuta.io>
2021-12-28 14:50:25 +0600

    Add

---
+++ Chapter 4/Listings/4.10strtype4.cpp
@@ -0,0 +1,27 @@
+//Listing 4.10 strtype4.cpp -- line input
+#include <iostream>
+#include <string>               // make string class available
+#include <cstring>              // C-style string library
+
+int main()
+{
+    using namespace std;
+    char charr[20];
+    string str;
+
+    cout << "Lenght of string in charr before input: " 
+        << strlen(charr) << endl;
+    cout << "Lenght of string in str before input: "
+        << str.size() << endl;
+    cout << "Enter a line of text:\n";
+    cin.getline(charr, 20);
+    cout << "You entered: " << charr << endl;
+    cout << "Enter another line of text:\n";
+    getline(cin, str);
+    cout << "Lenght of string in charr after input: "
+        << strlen(charr) << endl;
+    cout << "Lenght of string in str after input: "
+        << str.size() << endl;
+
+    return 0;
+}
